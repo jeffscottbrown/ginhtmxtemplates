92673a5592726529943881d606c5e68db7a1897e
Jeff Brown <brownj@objectcomputing.com>
2025-10-03 18:15:14 -0500

    updating documentation

--- ginhtmx/ginhtmx.go
+++ ginhtmx/ginhtmx.go
@@ -2,57 +2,61 @@
 // in Gin web applications.
 //
 // From your gin handler, you can use the RenderTemplate or RenderTemplateWithStatus
-// methods to render your templates. If the request includes the "Hx-Request" header
+// methods to render templates. If the request includes the "Hx-Request" header
 // indicating that it is an HTMX request, then the template will be rendered as-is.
 // If the request does not include that header, then the template will be wrapped in
-// a layout template.
+// a layout template.  This is useful for providing a consistent layout for
+// non-HTMX requests while still allowing HTMX requests to receive only the
+// content defined in the requested template.
 //
 // By default, the layout template is named "layout" and the body content is placed
-// in a variable named "Content". You can customize these names by using the
-// NewHtmxWithConfig function to create your Htmx instance.
+// in a variable named "Content". These names may be customized by using the
+// NewHtmxWithConfig function to create your Htmx instance.  The
+// NewHtmxWithConfig function takes a HtmxConfig struct which allows you to
+// specify the layout template name and body variable name.
 //
-// Here is an example of how to use ginhtmx in your Gin application:
+// Here is an example ofusing ginhtmx in a simple Gin application:
 //
-//		 package server
+//	package server
 //
-//		 import (
-//		   "embed"
-//		   "html/template"
+//	import (
+//	  "embed"
+//	  "html/template"
 //
-//		   "github.com/gin-gonic/gin"
-//		   "github.com/jeffscottbrown/ginhtmxtemplates/ginhtmx"
-//		 )
+//	  "github.com/gin-gonic/gin"
+//	  "github.com/jeffscottbrown/ginhtmxtemplates/ginhtmx"
+//	)
 //
-//		 //go:embed templates/*.html
-//		 var embeddedHTMLFiles embed.FS
+//	//go:embed templates/*.html
+//	var embeddedHTMLFiles embed.FS
 //
-//		 func Run() {
-//		   router := gin.Default()
+//	func Run() {
+//	  router := gin.Default()
 //
-//		   tmpl := template.Must(template.New("").Funcs(router.FuncMap).ParseFS(embeddedHTMLFiles, "templates/*.html"))
-//		   htmx := ginhtmx.NewHtmx(tmpl)
+//	  tmpl := template.Must(template.New("").Funcs(router.FuncMap).ParseFS(embeddedHTMLFiles, "templates/*.html"))
+//	  htmx := ginhtmx.NewHtmx(tmpl)
 //
-//		   handlers := &demoHandlers{htmx: htmx}
+//	  handlers := &demoHandlers{htmx: htmx}
 //
-//		   router.GET("/", handlers.home)
-//		   router.GET("/about", handlers.about)
+//	  router.GET("/", handlers.home)
+//	  router.GET("/about", handlers.about)
 //
-//		   router.Run(":8080")
-//		 }
+//	  router.Run(":8080")
+//	}
 //
-//	 func (handlers *demoHandlers) home(c *gin.Context) {
-//	   // Your handler logic here before rendering template
-//	   handlers.htmx.RenderTemplate(c, "home", gin.H{})
-//	 }
+//	func (handlers *demoHandlers) home(c *gin.Context) {
+//	  // Your handler logic here before rendering template
+//	  handlers.htmx.RenderTemplate(c, "home", gin.H{})
+//	}
 //
-//	   func (handlers *demoHandlers) about(c *gin.Context) {
-//	     // Your handler logic here before rendering template
-//	     handlers.htmx.RenderTemplate(c, "about", gin.H{})
-//	   }
+//	func (handlers *demoHandlers) about(c *gin.Context) {
+//	  // Your handler logic here before rendering template
+//	  handlers.htmx.RenderTemplate(c, "about", gin.H{})
+//	}
 //
-//	 type demoHandlers struct {
-//	   htmx *ginhtmx.Htmx
-//	 }
+//	type demoHandlers struct {
+//	  htmx *ginhtmx.Htmx
+//	}
 //
 // That server runs a simple hello word application with two routes, "/" and "/about".
 // The templates are embedded in the binary using the embed package.
